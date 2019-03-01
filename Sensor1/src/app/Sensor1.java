484411503b0bb69be6c8842c160d136047e598fc
Filipe de Andrade Morais <afilipem@gmail.com>
2019-03-01 17:29:01 -0600

    new: adding the constructor for sensor 1 and making notes of the developement process.

--- Sensor1/src/app/Sensor1.java
+++ Sensor1/src/app/Sensor1.java
@@ -8,8 +8,51 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.util.Random;
 import java.net.*;
 
+/*
+ * Classname
+ * 
+ * Version information
+ *
+ * Date
+ */
+
 //sensor of presence
 public class Sensor1 {
+
+    //atributes
+    public static final String BROKER_URL = "tcp://iot.eclipse.org:1883";
+    private MqttClient client;
+    String topic = "owner";
+
+    //constructor
+    //setting up the client
+    public Sensor1(){
+        String clientId = getMacAddress() + "-pub";
+        System.out.println("Client ID="+clientId);
+        try{
+            client = new MqttClient(BROKER_URL, clientId);
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+            final MqttTopic temperatureTopic = client.getTopic(topic);
+            options.setWill(temperatureTopic, "I'm gone".getBytes(), 2, true);
+            client.connect();
+        }catch (MqttException e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public byte[] getMacAddress(){
+        byte[] mac=new byte[6];
+        try{
+            InetAddress address = InetAddress.getLocalHost();
+            NetworkInterface nwi = NetworkInterface.getByInetAddress(address);
+            mac = nwi.getHardwareAddress();
+            System.out.println(mac);
+        }catch(Exception e) {System.out.println(e);}
+        return mac;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
     }
