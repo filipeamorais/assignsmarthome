239a1be65a32b54466f1d17c2cf81d02430fbdff
Filipe de Andrade Morais <afilipem@gmail.com>
2019-03-01 15:25:46 -0600

    new: adding the first files and setups for the sensors 1 and 2 implemented in Java.

---
+++ Sensor1/src/app/Sensor1.java
@@ -0,0 +1,16 @@
+package app;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttTopic;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import java.util.Random;
+import java.net.*;
+
+//sensor of presence
+public class Sensor1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
